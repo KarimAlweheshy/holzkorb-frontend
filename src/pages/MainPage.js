03bf2c900594e027dae248c7297669b218d9f575
kirill <cmirnov239kirill@gmail.com>
2020-07-16 18:18:50 +0200

    add basic main page

---
+++ src/pages/MainPage.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Carousel from "react-material-ui-carousel";
+import Grid from "@material-ui/core/Grid";
+import CardMedia from "@material-ui/core/CardMedia";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Card from "@material-ui/core/Card";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        paddingTop:20
+    },
+    media: {
+        height: 720,
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    }
+}));
+
+function Item(props) {
+    const classes = useStyles();
+    const handleClick = () => {
+        console.log("CLICK")
+    }
+    return (
+        <Grid>
+            <Card>
+                {/*<img src={path}/>*/}
+                <CardMedia
+                    className={classes.media}
+                    component="img"
+                    image={props.props}
+                    title="me"
+                    onClick={handleClick}
+                >
+
+                </CardMedia>
+            </Card>
+        </Grid>
+    )
+}
+
+export const MainPage = () => {
+    const classes = useStyles();
+    function importAll(r) {
+        return r.keys().map(r);
+    }
+    var listOfImages = importAll(require.context('../assets/img/main-page', false, /\.(png|jpe?g|svg)$/));
+    return (
+        <Grid className={classes.root} container>
+            <Grid item xs={2}/>
+            <Grid item xs={8}>
+                <Carousel>
+                    {
+                        listOfImages.map((src) => <Item props={src}/>)
+                    }
+                </Carousel>
+            </Grid>
+            <Grid item xs={2}/>
+        </Grid>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,45 +1,49 @@
 import React from 'react';
-import { Switch, Route, Redirect, Router } from 'react-router-dom';
-import { SearchPage } from './pages/SearchPage';
-import { AuthPage } from './pages/AuthPage';
-import { RegistrationPage } from './pages/RegistrationPage';
-import { RegistrationOkPage } from './pages/RegistrationOkPage';
+import {Switch, Route, Redirect, Router} from 'react-router-dom';
+import {SearchPage} from './pages/SearchPage';
+import {AuthPage} from './pages/AuthPage';
+import {RegistrationPage} from './pages/RegistrationPage';
+import {RegistrationOkPage} from './pages/RegistrationOkPage';
 import WelcomeFarmer from './pages/WelcomeFarmer';
 import ManageInventory from './pages/ManageInventory';
 import InventoryForm from './pages/InventoryForm';
-import { ProfilePage } from './pages/ProfilePage';
+import {ProfilePage} from './pages/ProfilePage';
 import history from './history';
 import ProductForm from './pages/ProductForm';
 import ManageProduct from './pages/ManageProduct';
+import {MainPage} from "./pages/MainPage";
 
 export const useRoutes = (isAuthenticated) => {
-  if (isAuthenticated) {
-    return (
-      <Switch>
-        <Route path="/profile" exact component={ProfilePage} />
-        <Route path="/search" exact component={SearchPage} />
-        <Route path="/registrationOk" exact component={RegistrationOkPage} />
-        <Route path="/welcome-farmer" exact component={WelcomeFarmer} />
-        <Route path="/manage-inventory" exact component={ManageInventory} />
-        <Route path="/inventory/:inventoryId" exact component={InventoryForm} />
-        <Route path="/manage-product" exact component={ManageProduct} />
-        <Route path="/product/:productId" exact component={ProductForm} />
-        <Redirect to="/" />
-      </Switch>
-    );
-  }
+    if (isAuthenticated) {
+        return (
+            <Switch>
+                <Route path="/profile" exact component={ProfilePage}/>
+                <Route path="/search" exact component={SearchPage}/>
+                <Route path="/registrationOk" exact component={RegistrationOkPage}/>
+                <Route path="/welcome-farmer" exact component={WelcomeFarmer}/>
+                <Route path="/manage-inventory" exact component={ManageInventory}/>
+                <Route path="/inventory/:inventoryId" exact component={InventoryForm}/>
+                <Route path="/manage-product" exact component={ManageProduct}/>
+                <Route path="/product/:productId" exact component={ProductForm}/>
+                <Redirect to="/"/>
+            </Switch>
+        );
+    }
 
-  return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact>
-          <AuthPage />
-        </Route>
-        <Route path="/registration">
-          <RegistrationPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route path="/" exact>
+                    <MainPage/>
+                </Route>
+                <Route path="/auth" exact>
+                    <AuthPage/>
+                </Route>
+                <Route path="/registration">
+                    <RegistrationPage/>
+                </Route>
+                <Redirect to="/"/>
+            </Switch>
+        </Router>
+    );
 };
